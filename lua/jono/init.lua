1374a29cccf0eacc98cd17e24fe90e1ae83cb4a3
Jonathan faller <jono.faller@gmail.com>
2023-12-13 20:13:10 +0200

    normal mode 'o' behaviour mod

--- lua/jono/init.lua
+++ lua/jono/init.lua
@@ -1,7 +1,16 @@
 require("jono.remap")
 require("jono.packer")
 require("jono.set")
+-- require("jono.lazy")
 
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = '*',
+    callback = function()
+        vim.opt.formatoptions:remove('o')
+    end
+})
+
