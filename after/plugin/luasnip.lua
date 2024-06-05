f85ebb7d72d20f2ca2a6bdc38f4150cf499f8e80
JonoLF <jono.faller@gmail.com>
2024-06-05 17:54:37 +0200

    new snippets

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -36,6 +36,53 @@ local date_input = function(args, snip, old_state, fmt)
 	return sn(nil, i(1, os.date(fmt_local)))
 end
 
+ls.add_snippets("all", {
+    s("if", {
+        t({"if ("}),
+        i(1,"conditions"),
+        t({") {"}),
+        t({"","\t"}),
+        i(2,"if code"),
+        t({"","}"}),
+    }),
+    s("ifelse", {
+        t({"if ("}),
+        i(1,"conditions"),
+        t({") {"}),
+        t({"","\t"}),
+        i(2,"if code"),
+        t({"","} else {"}),
+        t({"","\t"}),
+        i(3,"else code"),
+        t({"","}"}),
+    }),
+    s("elif", {
+        t({"else if ("}),
+        i(1,"conditions"),
+        t({") {"}),
+        t({"","\t"}),
+        i(2,"elif code"),
+        t({"","}"}),
+    }),
+    s("trycatch", {
+        t({"try {"}),
+        i(1,"code"),
+        t({"","} catch (error) {"}),
+        t({"","\t"}),
+        i(2,"catch code"),
+        t({"","}"}),
+    }),
+})
+ls.add_snippets("python", {
+    s("elif", {
+        t({"elif ("}),
+        i(1,"conditions"),
+        t({") {"}),
+        t({"","\t"}),
+        i(2,"elif code"),
+        t({"","}"}),
+    }),
+})
 ls.add_snippets("c", {
     s("fancycom", {
         t({"/****************************************", " * "}),
@@ -93,60 +140,135 @@ ls.add_snippets("c", {
         i(1, "user input"),
         t({"\\n\");", "printf(\"--------------------\\n\");"})
     }),
-}
-)
-
-ls.add_snippets("cpp", {
+})
+ls.filetype_set("cpp", { "c" });
+ls.add_snippets("javascript", {
     s("fancycom", {
         t({"/****************************************", " * "}),
         i(1, "COMMENT TEXT"),
         t({""," ****************************************/"}),
     }),
-    s("doxyfile", {
+    s("jsdocfile", {
         t({"/**"}),
-        t({""," * @file "}),
-        t({""," * @brief "}),
+        t({"","\t* @file "}),
+        t({"","\t* @brief "}),
         i(1, "Brief description of the file"),
-        t({""," *"}),
-        t({""," * @author "}),
+        t({"","\t*"}),
+        t({"","\t* @author "}),
         i(2, "Jonathan Faller"),
-        t({""," *"}),
-        t({""," * @date "}),
+        t({"","\t*"}),
+        t({"","\t* @date "}),
         d(3, date_input, {}, { user_args = { "%A, %B %d of %Y" } }),
-        t({""," */"}),
+        t({"","\t*/"}),
+        t({"",""}),
     }),
-    s("doxyfunc", {
-        t({"/**"}),
-        t({""," * @function "}),
-        t({""," * @brief "}),
-        i(1, "Brief description of the function"),
-        t({""," *"}),
-        t({""," * @param "}),
-        i(2, "PARAMS"),
-        t({""," *"}),
-        t({""," * @return "}),
-        i(3, "RETURNS"),
-        t({""," */"}),
+    s("jsdocfunc", {
+        t({"","/**"}),
+        t({"","\t* @function "}),
+        i(1, "name"),
+        t({"","\t* @brief "}),
+        i(2, "Brief description of the function"),
+        t({"","\t*"}),
+        t({"","\t* @param "}),
+        i(3, "PARAMS"),
+        t({"","\t*"}),
+        t({"","\t* @return "}),
+        i(4, "RETURNS"),
+        t({"","\t*/"}),
+        t({"",""}),
     }),
-    s("doxystruct", {
-        t({"/**"}),
-        t({""," * @struct "}),
-        t({""," * @brief "}),
-        i(1, "Brief description of the struct"),
-        t({""," */"}),
+    s("jsdocafunc", {
+        t({"","/**"}),
+        t({"","\t* @async "}),
+        t({"","\t* @function "}),
+        i(1, "name"),
+        t({"","\t* @brief "}),
+        i(2, "Brief description of the function"),
+        t({"","\t*"}),
+        t({"","\t* @param "}),
+        i(3, "PARAMS"),
+        t({"","\t*"}),
+        t({"","\t* @return "}),
+        i(4, "RETURNS"),
+        t({"","\t*/"}),
+        t({"",""}),
     }),
-    s("doxyvar", {
-        t({"/**"}),
-        t({""," * @var "}),
-        t({""," * @brief "}),
-        i(1, "Brief description of the var"),
-        t({""," */"}),
+    s("promise", {
+        t({"new Promise(async(resolve, reject) => {"}),
+        t({"","\t"}),
+        i(1, "code"),
+        t({"","});"}),
     }),
-    s("disprint", {
-        t({"printf(\"--------------------\\n\");", ""}),
-        t({"printf(\""}),
-        i(1, "user input"),
-        t({"\\n\");", "printf(\"--------------------\\n\");"})
+    s("apromise", {
+        t({"new Promise(async(resolve, reject) => {"}),
+        t({"","\t"}),
+        i(1, "code"),
+        t({"","});"}),
     }),
-}
-)
+    s("resolve", {
+        t({"resolve("}),
+        i(1, "fufillment"),
+        t({");"}),
+    }),
+    s("reject", {
+        t({"reject("}),
+        i(1, "reason"),
+        t({");"}),
+    }),
+    s("function", {
+        t({"function "}),
+        i(1,"name"),
+        t({"("}),
+        i(2,"parameters"),
+        t({") {"}),
+        t({"","\t"}),
+        i(3,"code"),
+        t({"","}"}),
+    }),
+    s("afunction", {
+        t({"async function "}),
+        i(1,"name"),
+        t({"("}),
+        i(2,"parameters"),
+        t({") {"}),
+        t({"","\t"}),
+        i(3,"code"),
+        t({"","}"}),
+    }),
+    s("error", {
+        t({"new Error("}),
+        i(1,"error"),
+        t({");"}),
+    }),
+    s("conlog", {
+        t({"console.log("}),
+        i(1,"log statement"),
+        t({");"}),
+    }),
+    s("conerr", {
+        t({"console.error("}),
+        i(1,"error statement"),
+        t({");"}),
+    }),
+    s(".catch", {
+        t({".catch((error) => {"}),
+        t({"","\tconsole.error(error.message)"}),
+        t({"","});"}),
+    }),
+    s(".then", {
+        t({".then(("}),
+        i(1,"value"),
+        t({") => {"}),
+        t({"","\t"}),
+        i(2, "code"),
+        t({"","})"}),
+    }),
+    s(".finally", {
+        t({".finally(("}),
+        i(1,"value"),
+        t({") => {"}),
+        t({"","\t"}),
+        i(2, "code"),
+        t({"","});"}),
+    }),
+})
