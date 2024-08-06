8990a2658527c2e6e74ae2d27ff1fc9f93cbc5a0
Jonathan Faller <jono.faller@gmail.com>
2024-08-06 22:31:30 +0200

    fixed some function expansion snippet stuff

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -26,12 +26,13 @@ require('mason-lspconfig').setup({
 local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
 local cmp_format = require('lsp-zero').cmp_format()
-
+require('luasnip.loaders.from_vscode')
 cmp.setup({
   sources = {
-    {name = 'nvim_lsp'},
-    {name = 'buffer'},
-    {name = 'luasnip'},
+      {name = 'path'},
+      {name = 'nvim_lsp'},
+      {name = 'buffer'},
+      {name = 'luasnip'},
   },
   --- (Optional) Show source name in completion menu
   window = {
@@ -39,12 +40,19 @@ cmp.setup({
       documentation = cmp.config.window.bordered(),
   },
   mapping = cmp.mapping.preset.insert({
+      ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+      ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-u>'] = cmp.mapping.scroll_docs(-4),
       ['<C-d>'] = cmp.mapping.scroll_docs(4),
       ['<C-y>'] = cmp.mapping.confirm({ select = true }),
   }),
   formatting = cmp_format,
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
 })
 
 lsp_zero.set_preferences({
