4e284739b808f3bc9d11e3c1e6c5e69442036de9
Jonathan Faller <jono.faller@gmail.com>
2024-09-19 20:57:03 +0200

    .

--- lua/jono/packer.lua
+++ lua/jono/packer.lua
@@ -198,7 +198,7 @@ return require('packer').startup(function(use)
         'nvim-telescope/telescope-fzf-native.nvim',
         run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' 
     }
-    use "sindrets/diffview.nvim" 
+    use "sindrets/diffview.nvim"
     use {
         "windwp/nvim-autopairs",
         event = "InsertEnter",
