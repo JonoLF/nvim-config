ebbac997189b469dce98c1750358fe053f8e495c
Jonathan Faller <jono.faller@gmail.com>
2024-09-26 11:14:04 +0200

    Changed merge tool layout

--- after/plugin/diffview.lua
+++ after/plugin/diffview.lua
@@ -37,7 +37,7 @@ require("diffview").setup({
     },
     merge_tool = {
       -- Config for conflicted files in diff views during a merge or rebase.
-      layout = "diff3_horizontal",
+      layout = "diff3_mixed",
       disable_diagnostics = true,   -- Temporarily disable diagnostics for diff buffers while in the view.
       winbar_info = true,           -- See |diffview-config-view.x.winbar_info|
     },
